14f851e875a2addf1ae26984ad92680d5b0a74cb
andrigame <andrigame1697@gmail.com>
2022-02-16 08:54:45 +0700

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -44,7 +44,7 @@ const Header = () => {
       <div className="header__wrap container">
         <div className="logo">
           <img src={logo} alt="logo"/>
-          <Link to="/">Tiduran</Link>
+          <Link to="/">mTiduran</Link>
         </div>
         <ul className="header__nav">
           {
